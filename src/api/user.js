b862e7d882bce5bd11ddf70d91c556b0cf4d107e
WildThorn <1343578324@qq.com>
2022-11-23 09:48:44 +0800

    前端框架登录改造（访问本地接口路径），后端服务新增模拟登陆接口(￣▽￣)／

--- src/api/user.js
+++ src/api/user.js
@@ -1,20 +1,32 @@
 import request from '@/utils/request'
 
+const api_name ='/admin/hosp/hospitalSet'
+
+/**
+ * 登陆
+ * @param {*} data 
+ * @returns 
+ */
 export function login(data) {
   return request({
-    url: '/vue-admin-template/user/login',
+    url: `${api_name}/login`,
     method: 'post',
     data
   })
 }
-
+/**
+ * 返回用户登陆信息
+ * @param {*} token 
+ * @returns 
+ */
 export function getInfo(token) {
   return request({
-    url: '/vue-admin-template/user/info',
+    url: `${api_name}/info`,
     method: 'get',
     params: { token }
   })
 }
+
 
 export function logout() {
   return request({
