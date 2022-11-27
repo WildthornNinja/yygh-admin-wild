ed278fcd18b2dad698376683e849769ac3446682
WildThorn <1343578324@qq.com>
2022-11-27 16:33:57 +0800

    dict/list.vue页面新增导入按钮对话框，实现文件上传导入数据功能ヾ(=･ω･=)o

--- src/views/yygh/dict/list.vue
+++ src/views/yygh/dict/list.vue
@@ -3,7 +3,7 @@
         <div class="el-toolbar">
             <div class="el-toolbar-body" style="justify-content: flex-start;">
                 <el-button type="text" @click="exportData"><i class="fa fa-plus"/> 导出</el-button>
-                
+                <el-button type="text" @click="importData"><i class="fa fa-plus"/> 导入</el-button>
             </div>
             
         </div>
@@ -50,6 +50,23 @@
         </el-table-column>
 
     </el-table>
+    <el-dialog title="导入" :visible.sync="dialogImportVisible" width="480px">
+            <el-form label-position="right" label-width="170px">
+                <el-form-item label="文件">
+                    <el-upload
+                            :multiple="false"
+                            :on-success="onUploadSuccess"
+                            :action="BASE_API"
+                            class="upload-demo">
+                        <el-button size="small" type="primary">点击上传</el-button>
+                        <div slot="tip" class="el-upload__tip">只能上传xls文件，且不超过500kb</div>
+                    </el-upload>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="dialogImportVisible = false">取消</el-button>
+            </div>
+        </el-dialog>
     </div>
 </template>
 <script>
@@ -58,7 +75,9 @@ export default{
     data(){
         return {
             listLoading: true, // 是否显示loading信息
-            list:[] //数据字典列表数组
+            list:[], //数据字典列表数组
+            BASE_API: `${process.env.VUE_APP_BASE_API}admin/cmn/dict/importData`,//上传地址
+            dialogImportVisible: false, //Dialog 对话框是否显示
         }
     },
     created(){
@@ -97,7 +116,26 @@ export default{
          exportData(){
             //内置window对象
             window.open(`${process.env.VUE_APP_BASE_API}admin/cmn/dict/exportData`);
-         }
+         },
+         /**
+          * 打开Dialog对话框
+          */
+          importData(){
+            this.dialogImportVisible = true
+          },
+          /**
+           * 上传数据，实现导入数据功能
+           */
+           onUploadSuccess(){
+            this.$message({
+                type:"success",
+                message: "上传成功!"
+            });
+            //关闭对话框
+            this.dialogImportVisible = false
+            //刷新数据
+            this.getDictList();
+           }
 
     }
 }
